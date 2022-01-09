51ae773cbe9bb0b45677b82c4ee742bb46e13d34
andrebolina <bolina.ac@gmail.com>
2022-01-09 19:40:56 -0300

    finalize server

--- server/tests/services/test_wellness_service.py
+++ server/tests/services/test_wellness_service.py
@@ -2,4 +2,10 @@ from src.services.wellness_service import calculate_wellness_score
 
 class TestWellnessService():
   def test_calculate_wellness_score_low_result(self):
-    assert calculate_wellness_score(1000, 80) == True
+    assert calculate_wellness_score(1000, 80) == 'LOW'
+
+  def test_calculate_wellness_score_medium_result(self):
+    assert calculate_wellness_score(1000, 30) == 'MEDIUM'
+
+  def test_calculate_wellness_score_healthy_result(self):
+    assert calculate_wellness_score(1000, 10) == 'HEALTHY'
