f4e2f377a850f097bc83f4517de1fc51d2de3fd2
andrebolina <bolina.ac@gmail.com>
2022-01-09 19:56:29 -0300

    add constants

--- server/tests/services/test_wellness_service.py
+++ server/tests/services/test_wellness_service.py
@@ -1,8 +1,13 @@
-from src.services.wellness_service import calculate_wellness_score
+import sys, os
+myPath = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, myPath + '/../../src/')
+
+from services.wellness_service import calculate_wellness_score
+from utils.constants.wellness_constants import LOW_SCORE
 
 class TestWellnessService():
   def test_calculate_wellness_score_low_result(self):
-    assert calculate_wellness_score(1000, 80) == 'LOW'
+    assert calculate_wellness_score(1000, 80) == LOW_SCORE
 
   def test_calculate_wellness_score_medium_result(self):
     assert calculate_wellness_score(1000, 30) == 'MEDIUM'
