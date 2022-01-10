07131b2a49ad52355af0ab6cebe7e08ad657c939
andrebolina <bolina.ac@gmail.com>
2022-01-09 23:50:30 -0300

    update

--- server/src/services/wellness_service.py
+++ server/src/services/wellness_service.py
@@ -9,15 +9,19 @@ MAX_MEDIUM_PERCENTAGE = 0.75
 MAX_HEALTHY_PERCENTAGE = 0.25
 
 
+def calculate_annual_net_compensation(annual_income: int):
+    return int(annual_income) - \
+        int(annual_income) * TAX_PERCENTAGE
+
+
 def calculate_wellness_score(annual_income, monthly_costs):
     annual_costs = int(monthly_costs) * 12
-    annual_compensation = int(annual_income) - \
-        int(annual_income) * TAX_PERCENTAGE
-    net_balance = annual_costs / annual_compensation
+    annual_net_compensation = calculate_annual_net_compensation(annual_income)
+    annual_net_balance = annual_costs / annual_net_compensation
 
-    if (net_balance > MAX_MEDIUM_PERCENTAGE):
+    if (annual_net_balance > MAX_MEDIUM_PERCENTAGE):
         return LOW_SCORE
-    elif (net_balance > MAX_HEALTHY_PERCENTAGE):
+    elif (annual_net_balance > MAX_HEALTHY_PERCENTAGE):
         return MEDIUM_SCORE
 
     return HEALTHY_SCORE
